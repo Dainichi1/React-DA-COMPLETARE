52371edf9f4f4504183193cd1b7eb76318d85a84
Dainichi1 <torquati79@yahoo.it>
2025-02-23 14:40:40 +0100

    39. Creating & Using a First Custom Component

--- Sezione3-ReactEssentials/src/App.jsx
+++ Sezione3-ReactEssentials/src/App.jsx
@@ -1,47 +1,22 @@
-import { CORE_CONCEPTS } from "./data";
-import Header from "./components/Header/Header";
-import CoreConcept from "./components/CoreConcept/CoreConcept";
-import TabButton from "./components/TabButton";
-import { useState } from "react"; // import the useState hook
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState(
-    "Please click a button to see the content"
-  ); // initialize the state with a default value
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
   return (
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-          {selectedTopic}
-        </section>
+        <h2>Time to get started!</h2>
       </main>
     </div>
   );
