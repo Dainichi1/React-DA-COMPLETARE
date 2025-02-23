8a1df3999e2f0483440b4e9a8d27da8573451f77
Dainichi1 <torquati79@yahoo.it>
2025-02-23 15:31:46 +0100

    49. Component Composition: The special "children" Prop [Core Concept]

--- Sezione3-ReactEssentials/src/App.jsx
+++ Sezione3-ReactEssentials/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
 import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/CoreConcepts/TabButton";
 
 function App() {
   return (
@@ -19,6 +20,15 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
