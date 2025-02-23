afef66018c180b8968c370add07d7afd1d1f1ccd
Dainichi1 <torquati79@yahoo.it>
2025-02-23 16:28:02 +0100

    58. Outputting List Data Dynamically - FINE SEZIONE 3

--- Sezione3-ReactEssentials/src/App.jsx
+++ Sezione3-ReactEssentials/src/App.jsx
@@ -18,15 +18,11 @@ function App() {
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
+
           <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]} />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcepts key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
