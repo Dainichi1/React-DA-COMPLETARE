e977b29ec76e7b304952ba47f8088937ebde2de4
Dainichi1 <torquati79@yahoo.it>
2025-02-23 15:43:34 +0100

    52. Passing Custom Arguments to Event Functions

--- Sezione3-ReactEssentials/src/App.jsx
+++ Sezione3-ReactEssentials/src/App.jsx
@@ -4,8 +4,8 @@ import { CORE_CONCEPTS } from "./data";
 import TabButton from "./components/CoreConcepts/TabButton";
 
 function App() {
-  function handleSelect() {
-    console.log("Tab selected");
+  function handleSelect(selecteBdButton) {
+    console.log(selecteBdButton);
   }
 
   return (
@@ -28,10 +28,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
         </section>
       </main>
