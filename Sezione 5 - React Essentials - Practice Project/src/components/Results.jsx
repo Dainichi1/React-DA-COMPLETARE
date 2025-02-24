ff7d4dd910d46a7f7fcca348982e4627fcc2f344
Dainichi1 <torquati79@yahoo.it>
2025-02-24 17:48:29 +0100

    102. Computing Values & Properly Handling Number Values

--- Sezione 5 - React Essentials - Practice Project/src/components/Results.jsx
+++ Sezione 5 - React Essentials - Practice Project/src/components/Results.jsx
@@ -1,3 +1,6 @@
+import { calculateInvestmentResults } from "../util/investment";
+
 export default function Results({ input }) {
+  const resultData = calculateInvestmentResults(input);
   return <p>Results...</p>;
 }
