677e33deaf3997357c5d71ccdf1e97dc6a326e70
Dainichi1 <torquati79@yahoo.it>
2025-02-24 14:39:32 +0100

    83. Avoid Intersecting States!

--- Sezione 4 - React essentials - Deep Dive/Tic Tac Toe/src/App.jsx
+++ Sezione 4 - React essentials - Deep Dive/Tic Tac Toe/src/App.jsx
@@ -1,12 +1,15 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
   function handleSelectSquare() {
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns();
   }
 
   return (
