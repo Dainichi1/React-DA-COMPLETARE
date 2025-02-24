2f6ff751991d39cb25b262c384104025f860ac97
Dainichi1 <torquati79@yahoo.it>
2025-02-24 18:00:22 +0100

    103. Outputting Results in a List & Deriving More Values - da rivedere

--- Sezione 5 - React Essentials - Practice Project/src/components/Results.jsx
+++ Sezione 5 - React Essentials - Practice Project/src/components/Results.jsx
@@ -1,6 +1,33 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ input }) {
-  const resultData = calculateInvestmentResults(input);
-  return <p>Results...</p>;
+  const resultsData = calculateInvestmentResults(input);
+  return (
+    <table id="result">
+      <thread>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thread>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const totalInterest =
+            year.valueEndOfYear - yearData.annualInvestment * yearData.year;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td></td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
