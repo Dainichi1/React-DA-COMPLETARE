8caa8e341efd441f88d7f2398e4454e2cb0127ff
Dainichi1 <torquati79@yahoo.it>
2025-02-28 11:24:41 +0100

    195. Deriving Values, Outputting Questions & Registering Answers

--- Sezione 12 - Practice Project Building a Quiz App/src/components/Quiz.jsx
+++ Sezione 12 - Practice Project Building a Quiz App/src/components/Quiz.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-  const [activeQuestion, setActiveQuestion] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+  //determina quale domanda del quiz deve essere mostrata in base al numero di risposte già date dall'utente.
 
-  return <p>Currently active Question</p>;
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((previousUserAnswers) => {
+      return [...previousUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
