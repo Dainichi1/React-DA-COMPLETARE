f136dbc991fd02a525465ee8963e6eba8feed9d7
Dainichi1 <torquati79@yahoo.it>
2025-03-13 16:09:47 +0100

    367. Error Handling with Custom Errors

--- Sezione 22 - Building a MultiPage SPA with React Router/1-progetto 2/frontend/src/App.js
+++ Sezione 22 - Building a MultiPage SPA with React Router/1-progetto 2/frontend/src/App.js
@@ -28,11 +28,13 @@ import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -45,6 +47,7 @@ const router = createBrowserRouter([
           {
             path: "",
             element: <EventsPage />,
+
             loader: eventsLoader,
           },
           {
