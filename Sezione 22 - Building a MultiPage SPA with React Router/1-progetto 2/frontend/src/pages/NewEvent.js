6aacfece443a568928bb87f34dc07aaa311fee77
Dainichi1 <torquati79@yahoo.it>
2025-03-13 17:17:06 +0100

    373. Working with action() Functions

--- Sezione 22 - Building a MultiPage SPA with React Router/1-progetto 2/frontend/src/pages/NewEvent.js
+++ Sezione 22 - Building a MultiPage SPA with React Router/1-progetto 2/frontend/src/pages/NewEvent.js
@@ -1,3 +1,4 @@
+import { redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
@@ -5,3 +6,33 @@ function NewEventPage() {
 }
 
 export default NewEventPage;
+
+export async function action({ request }) {
+  const formData = await request.formData(); // Aggiunto `await`
+
+  const eventData = {
+    title: formData.get("title"),
+    image: formData.get("image"),
+    date: formData.get("date"),
+    description: formData.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  // Controllo se la richiesta ha avuto successo
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "Failed to create event" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  // Se la richiesta è andata a buon fine, reindirizza alla pagina degli eventi
+  return redirect("/events");
+}
