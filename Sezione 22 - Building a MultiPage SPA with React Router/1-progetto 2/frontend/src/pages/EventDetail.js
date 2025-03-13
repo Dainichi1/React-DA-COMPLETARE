c19efa44285b4b9b99eb938b824d48b0c4b8e98d
Dainichi1 <torquati79@yahoo.it>
2025-03-13 17:30:02 +0100

    374. Submitting Data Programmatically

--- Sezione 22 - Building a MultiPage SPA with React Router/1-progetto 2/frontend/src/pages/EventDetail.js
+++ Sezione 22 - Building a MultiPage SPA with React Router/1-progetto 2/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
@@ -21,4 +21,17 @@ export async function loader({ request, params }) {
   }
 
   return response;
+}
+
+export async function action({ params }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: "DELETE",
+  });
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "Could not delete" }), {
+      status: 500,
+    });
+  }
+  return redirect("/events");
 }
