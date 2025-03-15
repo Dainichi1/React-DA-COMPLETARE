aed6c56a4dd4abab31214ce4fb7ce5ec4511a07d
Dainichi1 <torquati79@yahoo.it>
2025-03-15 15:34:39 +0100

    424. Optimistic Updating

--- Sezione 25 - React Query  Tanstack Query Handling HTTP Requests With Ease/src/components/Events/EditEvent.jsx
+++ Sezione 25 - React Query  Tanstack Query Handling HTTP Requests With Ease/src/components/Events/EditEvent.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { fetchEvent, updateEvent } from "../../util/http.js";
+import { fetchEvent, updateEvent, queryClient } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -17,6 +17,20 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: (data) => {
+      const newEvent = data.event;
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+      queryClient.setQueryData(["events", params.id], newEvent);
+
+      return { previousEvent: previousEvent };
+    },
+
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", params.id]);
+    },
   });
 
   function handleSubmit(formData) {
