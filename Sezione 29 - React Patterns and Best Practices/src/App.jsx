d46994b98c91bdbffcf924de867cebc83a1ba648
Dainichi1 <torquati79@yahoo.it>
2025-03-16 11:50:10 +0100

    541. Managing Multi-Component State with the Context API

--- Sezione 29 - React Patterns and Best Practices/src/App.jsx
+++ Sezione 29 - React Patterns and Best Practices/src/App.jsx
@@ -8,21 +8,46 @@ function App() {
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
           <AccordionItem
+            id="experience"
             className="accordion-item"
             title="We got 20 years of experience"
           >
             <article>
               <p>Lorem Ipsum</p>
-              <p>LOREM IPSUM</p>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
             </article>
           </AccordionItem>
           <AccordionItem
+            id="local"
             className="accordion-item"
             title="We're working with local guides"
           >
             <article>
               <p>Lorem Ipsum</p>
-              <p>LOREM IPSUM</p>
+              <p>
+                "At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas molestias excepturi sint occaecati cupiditate
+                non provident, similique sunt in culpa qui officia deserunt
+                mollitia animi, id est laborum et dolorum fuga. Et harum quidem
+                rerum facilis est et expedita distinctio. Nam libero tempore,
+                cum soluta nobis est eligendi optio cumque nihil impedit quo
+                minus id quod maxime placeat facere possimus, omnis voluptas
+                assumenda est, omnis dolor repellendus. Temporibus autem
+                quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+                eveniet ut et voluptates repudiandae sint et molestiae non
+                recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
+                ut aut reiciendis voluptatibus maiores alias consequatur aut
+                perferendis doloribus asperiores repellat."
+              </p>
             </article>
           </AccordionItem>
         </Accordion>
