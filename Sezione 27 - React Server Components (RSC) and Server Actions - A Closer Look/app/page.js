d869a154b190c82e13884788457838441fc5f109
Dainichi1 <torquati79@yahoo.it>
2025-03-16 10:45:07 +0100

    515. Handling Errors

--- Sezione 27 - React Server Components (RSC) and Server Actions - A Closer Look/app/page.js
+++ Sezione 27 - React Server Components (RSC) and Server Actions - A Closer Look/app/page.js
@@ -1,12 +1,15 @@
 import UsePromiseDemo from "@/components/UsePromisesDemo";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <main>
-      <Suspense fallback={<p>Loading...</p>}>
-        <UsePromiseDemo />
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<p>Loading...</p>}>
+          <UsePromiseDemo />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
