94257f32c4b695cbdc6db05f2ac1a76d2674a3f9
Dainichi1 <torquati79@yahoo.it>
2025-03-16 11:34:29 +0100

    540. Introducing Compound Components

--- Sezione 29 - React Patterns and Best Practices/src/App.jsx
+++ Sezione 29 - React Patterns and Best Practices/src/App.jsx
@@ -1,5 +1,34 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            className="accordion-item"
+            title="We got 20 years of experience"
+          >
+            <article>
+              <p>Lorem Ipsum</p>
+              <p>LOREM IPSUM</p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            className="accordion-item"
+            title="We're working with local guides"
+          >
+            <article>
+              <p>Lorem Ipsum</p>
+              <p>LOREM IPSUM</p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
